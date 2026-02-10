291395b513d6c3b2aba4ffc71f5368f408afe396
Vik <vik@vikonkeys.com>
2026-02-10 23:28:14 +0000

    Initial Commit

---
+++ HomeMini/HomeMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeMiniApp.swift
+//  HomeMini
+//
+//  Created by Vik  on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct HomeMiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
